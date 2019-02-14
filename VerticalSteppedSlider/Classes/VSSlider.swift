caa1ee02c68d845598525dbad53a3d0c86b04408
Gil Nakache <gil.nakache@viseo.com>
2019-02-14 15:42:34 +0100

    Cancelling addTarget deletion

--- VerticalSteppedSlider/Classes/VSSlider.swift
+++ VerticalSteppedSlider/Classes/VSSlider.swift
@@ -271,6 +271,10 @@ public class VSSlider: UIControl {
             }
         }
     }
+
+    override public func addTarget(_ target: Any?, action: Selector, for controlEvents: UIControl.Event) {
+        slider.addTarget(target, action: action, for: controlEvents)
+    }
     
     public override func draw(_ rect: CGRect) {
         // Redraw track images if the slider size has changed
